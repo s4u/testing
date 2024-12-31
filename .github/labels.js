4bf315ba219f5fc4d4192c122804fa57ddb256b5
Slawomir Jaranowski <s.jaranowski@gmail.com>
2024-12-31 17:13:52 +0100

    Label sync

---
+++ .github/labels.js
@@ -0,0 +1,17 @@
+
+const labels = [
+    {
+        name: 'bug',
+        color: 'B60205',
+        description: 'Something isn\'t working.'
+    },
+    {
+        name: 'duplicate',
+        color: 'cfd3d7',
+        description: 'This issue or pull request already exists.'
+    }
+];
+
+module.exports = {
+    labels
+}
